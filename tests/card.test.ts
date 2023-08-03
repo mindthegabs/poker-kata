a21bea3c117305e29e96775f2bc37837342186a1
g.aichinger <gabriel.aichinger@gmail.com>
2023-08-03 10:39:33 +0200

    Adds card validation tests.

--- tests/card.test.ts
+++ tests/card.test.ts
@@ -1,7 +1,44 @@
+import {Card} from '../src/card';
 
-describe('test', () => {
-    it('test', () => {
-        expect(true).toEqual(true);
+describe('Card', () => {
+    it('should create a card with the specified identifier', () => {
+        const identifier = 'As';
+        const expectedValue = 'A';
+        const expectedSuit = 's';
+
+        const card = new Card(identifier);
+
+        expect(card.value).toBe(expectedValue);
+        expect(card.suit).toBe(expectedSuit);
+
+    });
+
+    it('should throw an error for an invalid card value', () => {
+        const identifier = 'Xh';
+        const expectedError = 'Invalid card value';
+
+        const result = () => new Card(identifier);
+
+        expect(result).toThrowError(expectedError);
+    });
+
+    it('should throw an error for an invalid card suit', () => {
+        const identifier = '2P';
+        const expectedError = 'Invalid card suit';
+
+        const result = () => new Card(identifier);
+
+        expect(result).toThrowError(expectedError);
+    });
+
+
+    it('should throw an error for an empty identifier', () => {
+        const identifier = '';
+        const expectedError = 'Invalid card value';
+
+        const result = () => new Card(identifier);
+
+        expect(result).toThrowError(expectedError);
     });
 
 });
